a14fd5a8018960ab966557e725120f315b3960bd
tron135 <myash180@gmail.com>
2018-03-02 00:23:41 +0530

    files added

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require ("express");
+var app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function(req,res){
+    res.render("home.ejs");
+});
+
+app.get("/fallinlovewith/:thing", function(req,res){
+    var thing = req.params.thing;
+    res.send("You fell in love with " + thing);
+});
+
+app.get("/posts", function(req,res){
+    var posts = [
+        {title: "Post 1", author: "Susy"},
+        {title: "My adorable pet bunny", author: "Charlie"},
+        {title: "Can you believe this pomsky", author: "Colt"}
+    ];
+    res.render("posts.ejs", {posts: posts});
+});
+
+app.listen(3000, function(){
+    console.log("Server is listening!!!");
+});
